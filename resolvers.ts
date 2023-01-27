c7ca2c8c84df8e31434d92128fcfd77c772568c8
Oleh Abanin <oleh@outvio.com>
2023-01-27 09:41:40 +0200

    fix: input

--- resolvers.ts
+++ resolvers.ts
@@ -1,14 +1,38 @@
 import Jobs from "./models/jobs";
-import jobs from "./models/jobs";
 import Companies from "./models/companies";
 
 const resolvers = {
   Query: {
+    company: (root: any, args: { id: string }) => {
+      return Companies.findById(args.id).exec();
+    },
+    job: (root: any, args: { id: string }) => Jobs.findById(args.id).exec(),
     jobs: () => Jobs.find({}).exec(),
   },
+  Mutation: {
+    createJob: (
+      root: any,
+      {
+        input,
+      }: { input: { title: string; description: string; companyId: string } }
+    ) => {
+      const job = new Jobs({
+        title: input.title,
+        description: input.description,
+        compId: input.companyId,
+      });
+      job.save();
+      return job;
+    },
+  },
   Job: {
-    company: async (job: { _id: string; compId: string; title: string }) => {
+    company: async (job: { compId: string }) => {
       return Companies.findById(job.compId).exec();
+    },
+  },
+  Company: {
+    jobs: async (company: { _id: string }) => {
+      return Jobs.find({ compId: company._id }).exec();
     },
   },
 };
